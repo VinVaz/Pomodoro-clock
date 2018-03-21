0f6e41d030c523dde5674e6b631d487d4553c352
VinVaz <viniciusvazpro@gmail.com>
2018-03-21 16:51:39 -0300

    demo timer

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-document.getElementById("main").innerHTML = "HELLO WORLD"
+var pomodoroClock = setInterval(everySecond, 1000);
+const startingTime = 5;
+var time = 0;
+var increment = 0;
+
+function everySecond(){
+	time = startingTime - increment;
+	document.getElementById("main").innerHTML = time.toString();
+	if(startingTime==increment){
+	clearInterval(pomodoroClock);
+    }
+	increment++;
+}
+
