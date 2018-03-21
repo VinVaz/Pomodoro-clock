21054529e812fe7ad149f4ccaf9aa2b09e873d9f
VinVaz <viniciusvazpro@gmail.com>
2018-03-21 19:00:51 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,14 +1,25 @@
 var pomodoroClock = setInterval(everySecond, 1000);
-const startingTime = 5;
+const startingTime = 0;
+var seconds = 0;
+var hour = 0;
 var time = 0;
 var increment = 0;
 
+
+
 function everySecond(){
-	time = startingTime - increment;
-	document.getElementById("main").innerHTML = time.toString();
-	if(startingTime==increment){
-	clearInterval(pomodoroClock);
+	if(seconds==60){
+		seconds = 0;
+		hour++;
+	}
+	if(seconds.toString().length<2) seconds = "0" + seconds;
+	if(hour.toString().length<2) hour = "0" + hour;
+	time = hour + ":" + seconds;
+	document.getElementById("main").innerHTML = time;
+	if(time=="02:00"){
+		clearInterval(pomodoroClock);
     }
-	increment++;
+	//increment++;
+	seconds++;
 }
 
