1e70b143e7c3cd066d6b6fa06aab8d313d136e81
VinVaz <viniciusvazpro@gmail.com>
2018-03-25 22:44:24 -0300

    trying to get the minus and plus button to start only after the timer is paused

--- js/index.js
+++ js/index.js
@@ -1,14 +1,27 @@
-var pomodoroClock = setInterval(everySecond, 1000);
-const startTime = "23:00";
+//global variables
 const breakTime = "00:00";
-var timeArr = startTime.split(":");
-var mySeconds = timeArr[1];
-var myMinutes = timeArr[0];
+var hasInterval = false;
+var pomodoroClock;
+var timeArr = [];
+var mySeconds = 0;
+var myMinutes = 0;
 var myTime = 0;
+var setOfDigits = "";
+//sets the time at wich the clock will start
+function startPomodoroAt(startTime){
+	timeArr = startTime.split(":");
+	mySeconds = timeArr[1];
+	myMinutes = timeArr[0];
 
-clearNumbers();
+	clearNumbers();
+	setOfDigits = myMinutes.toString() + mySeconds.toString();
+	turnNumToDigital(setOfDigits);
+}
+//by dafault:
+startPomodoroAt("25:00");
 //the main function that makes the clock work
-function everySecond(){
+function doEveryScond(){
+	  mySeconds--;
 	  if(mySeconds<0){
 		mySeconds = 59;
 		myMinutes--;
@@ -16,26 +29,26 @@ function everySecond(){
 	  if(mySeconds.toString().length<2) mySeconds = "0" + mySeconds;
 	  if(myMinutes.toString().length<2) myMinutes = "0" + myMinutes;
 	myTime = myMinutes + ":" + mySeconds;
-	var setOfDigits = myMinutes.toString() + mySeconds.toString();
+	setOfDigits = myMinutes.toString() + mySeconds.toString();
 	turnNumToDigital(setOfDigits);
 	  if(myTime==breakTime){
 		clearInterval(pomodoroClock);
-      }
-	mySeconds--;
+	  }
+	
 }
 //stops or starts the counter when the button's pressed
-var hasInterval = true;
 document.getElementById("startButton").addEventListener("click", function(){
-    if(hasInterval){
+	if(hasInterval){
 		clearInterval(pomodoroClock);
-	    hasInterval = false;
+		hasInterval = false;
 	}
 	else{
-	    pomodoroClock = setInterval(everySecond, 1000);
+		pomodoroClock = setInterval(doEveryScond, 1000);
 		hasInterval = true;
 	}
 })
 //gets a set of digits thats represents the time and turn the into digital number on display
+//setOfDigits must be a string of numbers
 function turnNumToDigital(setOfDigits){
     var id = "";
 	var arr =setOfDigits.split("");
@@ -43,10 +56,9 @@ function turnNumToDigital(setOfDigits){
 	clearNumbers();
 	for(var i = 0; i < arr.length; i++){
 	  id = "num" + arr[i] + "digit" + (i+1);
-	  putNumber.visibleById(id);
+	  visibleById(id);
     }
 }
-
 //clear sgv elements by their ID
 function clearById(id){
     document.getElementById(id).setAttribute("style", "visibility: hidden");
@@ -57,18 +69,14 @@ function visibleById(id){
 //clear all the numbers from the timer display	
 function clearNumbers(){
    var id = "";
+   var limit = 5;   
    for(var i = 0; i <10; i++){
-	   for(var j = 1; j < 5; j++){
+	   for(var j = 1; j < limit; j++){
 		   id = "num" + i +"digit" + j;
 		   clearById(id);
 		   id = "";
 	   }
    }
-}
-var putNumber = {
-	visibleById: function(id){
-		visibleById(id)
-    }
 }
 
 //DEMO
@@ -81,7 +89,7 @@ function turnSessionNumToDigital(setOfDigits){
     clearSessionNumbers();
 	for(var i = 0; i < arr.length; i++){
 	  id = "num" + arr[i] + "digit" + (i+1) + "session";
-	  putNumber.visibleById(id);
+	  visibleById(id);
     }
 }
 function clearSessionNumbers(){
@@ -94,25 +102,31 @@ function clearSessionNumbers(){
 	   }
    }
 }
-var atp = 24;
-turnSessionNumToDigital("25")
+
+var sessionTime = 25;
+turnSessionNumToDigital(sessionTime.toString());
 var anyTime = 0;
-document.getElementById("plusSessionButton").onclick = function(){
-	if(atp>25) atp = 25;
-	if(atp<0) atp = 0;
-		anyTime = atp;
-		if(anyTime.toString().length<2) anyTime = "0" + anyTime;
-		else anyTime = anyTime.toString();
-		turnSessionNumToDigital(anyTime);
-		atp++;	
+function filterSessionTime(){
+	if(sessionTime>25) sessionTime = 25;
+	else if(sessionTime<0) sessionTime = 0;
+	else {
+		stringSessionTime = sessionTime.toString();
+		if(stringSessionTime.length<2) stringSessionTime = "0" + stringSessionTime;
+		turnSessionNumToDigital(stringSessionTime);
+		//startPomodoroAt(stringSessionTime+":00");
+	}
 }
-document.getElementById("minusSessionButton").onclick = function(){
-	if(atp>25) atp = 25;
-	if(atp<0) atp = 0;
-		anyTime = atp;
-		if(anyTime.toString().length<2) anyTime = "0" + anyTime;
-		else anyTime = anyTime.toString();
-		turnSessionNumToDigital(anyTime);
-		atp--;	
+if(hasInterval==false){
+	document.getElementById("plusSessionButton").onclick = function(){
+		filterSessionTime();
+		sessionTime++;	
+	}
+	document.getElementById("minusSessionButton").onclick = function(){
+		filterSessionTime();
+		sessionTime--;		
+	}
+}else{
+	filterSessionTime();
 }
+
 //END of DEMO
